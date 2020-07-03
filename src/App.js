b7ec3f02bf46e676d24263da3c59e67f00c08cb9
Bryan <blindaa121@gmail.com>
2020-07-02 23:22:43 -0700

    useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  // const [{count, count2}, setCount] = useState({count: 10, count2: 20});
+  const [count, setCount] = useState(10);
+  const [count2, setCount2] = useState(20);
   return (
     <div className="App">
-      <h1>hello</h1>
-    </div>
-  );
+        <h1>Count 1: {count}</h1>
+        <button onClick={() => setCount(c => c + 1)}>+</button>
+        <h1>Count 2: {count2}</h1>
+        <button onClick={() => setCount2(c => c + 1)}>+</button>
+     </div>
+  )
+    
+
 }
 
 export default App;
