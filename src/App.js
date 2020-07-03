74ee72f44f93dd1e4022ce54d82bb527e3a8ed51
Bryan <blindaa121@gmail.com>
2020-07-03 12:39:09 -0700

    Create custom hook useForm

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import {useForm} from './useForm'
 
 const App = () => {
   // const [{count, count2}, setCount] = useState({count: 10, count2: 20});
-  const [count, setCount] = useState(10);
-  const [count2, setCount2] = useState(20);
+  const [values, handleChange] = useForm({email: '', password: ''})
+  // const [password, setPassword] = useState('');
+
+  useEffect(() => { // function will get called everytime the component renders
+    console.log('render')
+  }, [values.password])
+
   return (
     <div className="App">
-        <h1>Count 1: {count}</h1>
-        <button onClick={() => setCount(c => c + 1)}>+</button>
-        <h1>Count 2: {count2}</h1>
-        <button onClick={() => setCount2(c => c + 1)}>+</button>
+        <input
+          name='email'
+          value={values.email}
+          onChange={handleChange}/>
+        <input
+          name='password'
+          type='password'
+          value={values.password}
+          onChange={handleChange}/>
      </div>
   )
     
